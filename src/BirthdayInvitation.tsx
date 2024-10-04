b7106b7b31334369d7d13115ebb2885266930d72
Ian <iancunha20@gmail.com>
2024-10-04 20:50:25 -0300

    Update BirthdayInvitation.tsx

--- src/BirthdayInvitation.tsx
+++ src/BirthdayInvitation.tsx
@@ -9,58 +9,72 @@ interface BirthdayInvitationProps {
   phoneNumber: string;
 }
 
-const BirthdayInvitation: React.FC<BirthdayInvitationProps> = ({ name, date, location, style, phoneNumber }) => {
+const BirthdayInvitation: React.FC<BirthdayInvitationProps> = ({
+  name,
+  date,
+  location,
+  style,
+  phoneNumber,
+}) => {
   const handleRsvp = () => {
     const message = `Olá! Confirmo minha presença no aniversário de ${name} no dia ${date}, no local ${location}!!!`;
     const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-    
+
     window.open(whatsappLink, '_blank');
   };
 
   return (
     <div style={style}>
-      <img 
+      <img
         src={ravena}
-        alt="Aniversário" 
-        style={styles.image}
+        alt="Aniversário"
+        style={{
+          width: '200px',
+          height: '200px',
+          borderRadius: '50%',
+          display: 'block',
+          margin: '0 auto 20px auto',
+        }}
       />
-      <h1 style={styles.title}>Você está convidado para o aniversário de {name}!</h1>
-      <p style={styles.details}><strong>Data:</strong> {date}</p>
-      <p style={styles.details}><strong>Local:</strong> {location}</p>
-      <button style={styles.button} onClick={handleRsvp}>Confirmar Presença</button>
+      <h1 style={{
+        fontSize: '3em',
+        fontWeight: 'bold',
+        marginBottom: '10px',
+        textAlign: 'center',
+      }}>
+        Você está convidado para o aniversário de {name}!
+      </h1>
+      <p style={{
+        fontSize: '1.3em',
+        marginBottom: '25px',
+        textAlign: 'center',
+      }}>
+        <strong>Data:</strong> {date}
+      </p>
+      <p style={{
+        fontSize: '1.3em',
+        marginBottom: '25px',
+        textAlign: 'center',
+      }}>
+        <strong>Local:</strong> {location}
+      </p>
+      <button
+        style={{
+          padding: '20px 20px',
+          fontSize: '20px',
+          backgroundColor: '#af4c99',
+          color: 'white',
+          border: 'none',
+          borderRadius: '5px',
+          cursor: 'pointer',
+          width: '100%',
+        }}
+        onClick={handleRsvp}
+      >
+        Confirmar Presença
+      </button>
     </div>
   );
-};
-
-const styles = {
-  image: {
-    width: '200px',
-    height: '200px',
-    borderRadius: '50%',
-    display: 'block',
-    margin: '0 auto 20px auto',
-  },
-  title: {
-    fontSize: '3em',
-    fontWeight: 'bold',
-    marginBottom: '10px',
-    textAlign: 'center',
-  },
-  details: {
-    fontSize: '1.3em',
-    marginBottom: '25px',
-    textAlign: 'center',
-  },
-  button: {
-    padding: '20px 20px',
-    fontSize: '20px',
-    backgroundColor: '#af4c99',
-    color: 'white',
-    border: 'none',
-    borderRadius: '5px',
-    cursor: 'pointer',
-    width: '100%',
-  }
 };
 
 export default BirthdayInvitation;
